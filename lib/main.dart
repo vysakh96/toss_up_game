f9dca01ccfb1afa3f17075e92f216a3512933e2e
vysakh96 <118036322+vysakh96@users.noreply.github.com>
2022-12-30 19:19:35 +0530

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false,
-  home: const TossUp(),),
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const TossUp(),
+    ),
   );
 }
+
 class TossUp extends StatefulWidget {
   const TossUp({Key? key}) : super(key: key);
 
@@ -18,46 +21,58 @@ class TossUp extends StatefulWidget {
 class _TossUpState extends State<TossUp> {
   int number1 = 1;
   int number2 = 1;
-  void display(){
-    number1 = Random().nextInt(6)+1;
-    number2 = Random().nextInt(6)+1;
+  void display() {
+    number1 = Random().nextInt(6) + 1;
+    number2 = Random().nextInt(6) + 1;
     setState(() {
       number1;
       number2;
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Toss Up Game',style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold,color: Colors.red),),
-      centerTitle: true,
-      backgroundColor: Colors.black,),
+      appBar: AppBar(
+        title: Text(
+          'Toss Up Game',
+          style: TextStyle(
+              fontSize: 30, fontWeight: FontWeight.bold, color: Colors.red),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
       backgroundColor: Colors.red[900],
-      body: Align(alignment:Alignment.center ,
+      body: Align(
+        alignment: Alignment.center,
         child: Padding(
           padding: const EdgeInsets.all(10.0),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              InkWell(onTap: () {
-                 display();
-              },
+              InkWell(
+                onTap: () {
+                  display();
+                },
                 child: Container(
                   height: 100,
                   width: 100,
                   decoration: BoxDecoration(
-                    image: DecorationImage(image: AssetImage('images/dice $number1.png')),
+                    image: DecorationImage(
+                        image: AssetImage('images/dice $number1.png')),
                   ),
                 ),
               ),
-              InkWell(onTap: () {
+              InkWell(
+                onTap: () {
                   display();
-              },
+                },
                 child: Container(
                   height: 100,
                   width: 100,
                   decoration: BoxDecoration(
-                    image: DecorationImage(image: AssetImage('images/dice $number2.png')),
+                    image: DecorationImage(
+                        image: AssetImage('images/dice $number2.png')),
                   ),
                 ),
               ),
@@ -65,9 +80,6 @@ class _TossUpState extends State<TossUp> {
           ),
         ),
       ),
-
     );
   }
 }
-
-
